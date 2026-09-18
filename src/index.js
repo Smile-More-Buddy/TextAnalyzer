6e36061a219f19b906acf89f6a1f080858371351
the-creative-one <darshrutidz@gmail.com>
2026-09-18 20:23:09 +0530

    Fix GitHub Pages routing

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/TextAnalyzer">
     <App />
   </BrowserRouter>,
 );
-
